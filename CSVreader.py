abacaedd9aca1ec89b2e0ed107067b14624cb5b9
Sebastian Rojas Salazar <RSsebas08@outlook.com>
2023-03-08 18:16:56 -0600

    Obtained data frame from spreadsheet

--- CSVreader.py
+++ CSVreader.py
@@ -1,11 +1,12 @@
 import gspread
+import pandas as pd
 
 gc = gspread.oauth(
     credentials_filename='Credentials.json'
 )
 
-sh = gc.open("Example spreadsheet")
-
-print(sh.sheet1.get('B1'))
+sh = gc.open_by_url("https://docs.google.com/spreadsheets/d/1m0qPA23PW2tQL1rrStTBrN9H1SpMvPg-Y-HiWd0FLcE/edit?usp=sharing")
+dataframe = pd.DataFrame(sh.sheet1.get_all_records())
+print(dataframe)
 
 
