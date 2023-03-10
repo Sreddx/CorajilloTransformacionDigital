7f3ed0ef55df68ef169a2a687a39ffdf60d8d657
Sebastian Rojas Salazar <RSsebas08@outlook.com>
2023-03-09 20:33:24 -0600

    Refactor a funciones y mejora de codigo

--- CSVreader.py
+++ CSVreader.py
@@ -2,97 +2,138 @@ import gspread
 import pandas as pd
 import psycopg2
 
-gc = gspread.oauth(
-    credentials_filename='Credentials.json'
-)
-#SH es la hoja en la que estamos trabajando
-sh = gc.open_by_url("https://docs.google.com/spreadsheets/d/1m0qPA23PW2tQL1rrStTBrN9H1SpMvPg-Y-HiWd0FLcE/edit?usp=sharing")
-sheets = sh.worksheets()
+def get_spreadsheet_data(credentials_filename, url):
+    gc = gspread.oauth(credentials_filename='Credentials.json')
+    sh = gc.open_by_url(url)
+    sheets = sh.worksheets()
 
-# Creamos un diccionario vacío para guardar los dataframes
-dfs = {}
+    dfs = {}
+    for sheet in sheets:
+        data = sheet.get_all_records()
+        df = pd.DataFrame(data)
 
-# Iteramos sobre todas las hojas
-for sheet in sheets:
-    # Obtenemos los datos de la hoja y creamos un dataframe
-    data = sheet.get_all_records()
-    df = pd.DataFrame(data)
+        # Limpiamos los datos de la hoja
+        clean_dataframe(df)
+
+        dfs[sheet.title] = df
+    
+    return dfs
+
+def clean_dataframe(df):
+    # Cambiamos el nombre de las columnas para que hagan match con la base de datos
+    if 'PIEZAS' in df.columns:
+        df.rename(columns={
+            'GAMA': 'Gama',
+            'ARTICULO-DESCRIPCION': 'Articulo_Descripcion',
+            'FOTO': 'Foto',
+            'COSTO': 'Costo',
+            'PIEZAS': 'Piezas',
+            'TOTAL ($)': 'Total'
+        }, inplace=True)
+    else:
+        df.rename(columns={
+            'GAMA': 'Gama',
+            'ARTICULO-DESCRIPCION': 'Articulo_Descripcion',
+            'FOTO': 'Foto',
+            'COSTO': 'Costo',
+            'TARIMAS': 'Tarimas',
+            'TOTAL ($)': 'Total'
+        }, inplace=True)
     
     # Quitamos los signos de dinero para facilitar manipulacion de datos
-    if df['COSTO'].dtype == 'object':
-        df['COSTO'] = df['COSTO'].str.replace('$', '', regex=False).str.replace(',', '', regex=False)
-    if df['TOTAL ($)'].dtype == 'object':
-        df['TOTAL ($)'] = df['TOTAL ($)'].str.replace('$', '',regex=False).str.replace(',', '', regex=False)
+    if df['Costo'].dtype == 'object':
+        df['Costo'] = df['Costo'].str.replace('$', '', regex=False).str.replace(',', '', regex=False)
+    if df['Total'].dtype == 'object':
+        df['Total'] = df['Total'].str.replace('$', '',regex=False).str.replace(',', '', regex=False)
 
-    #Convertirmos columnas de dinero a float
-    df['COSTO'] = df['COSTO'].astype(float)
-    df['TOTAL ($)'] = df['TOTAL ($)'].astype(float)
+    #Convertimos columnas de dinero a float
+    df['Costo'] = df['Costo'].astype(float)
+    df['Total'] = df['Total'].astype(float)
+
     
-    # Guardamos el dataframe en el diccionario
-    dfs[sheet.title] = df
 
-#print(df)
-worksheet_list = sh.worksheets()
-print(dfs)
-#Cambiamos el nombre de las columnas del Df para que al insertar a la base de datos sea mas sencillo.
-# df = df.rename(columns={
-#     'GAMA': 'Gama',
-#     'ARTICULO-DESCRIPCION': 'Articulo_Descripcion',
-#     'FOTO': 'Foto',
-#     'COSTO': 'Costo',
-#     'PIEZAS': 'Piezas',
-#     'TOTAL ($)': 'Total'
-# })
+def create_database_table(conn):
+    cur = conn.cursor()
 
+    # Antes de crear la base de datos, borramos la tabla en caso de que exista
+    cur.execute("DROP TABLE IF EXISTS inventario;")
 
+    # Creamos la tabla
+    cur.execute('''
+        CREATE TABLE inventario (
+            id SERIAL PRIMARY KEY,
+            Tipo VARCHAR(20),
+            Gama VARCHAR(255),
+            Articulo_Descripcion VARCHAR(255),
+            Foto VARCHAR(255),
+            Costo DECIMAL(10,2),
+            Piezas INTEGER,
+            Tarimas FLOAT,
+            Total DECIMAL(10,2)
+        );
+    ''')
 
-# # Nos conectamos a la base de datos utilizando las credenciales.
-# # Connect to an existing database
-# conn = psycopg2.connect(
-#     host="pg.pg4e.com",
-#     port=5432,
-#     database="pg4e_d5cf41ba12", 
-#     user="pg4e_d5cf41ba12",
-#     password="pg4e_p_34432ea43bce4f4")
+    conn.commit()
+    cur.close()
 
-# # Abrimos un cursor para realizar operaciones en la base de datos
-# cur = conn.cursor()
+def populate_database_table(conn, df):
+    cur = conn.cursor()
 
+    for _, row in df.iterrows():
+        if 'Tarimas' not in df.columns:
+            cur.execute("""
+                INSERT INTO inventario (Tipo, Gama, Articulo_Descripcion, Foto, Costo, Piezas, Total)
+                VALUES (%s, %s, %s, %s, %s, %s, %s);
+            """, ("", row['Gama'], row['Articulo_Descripcion'], row['Foto'], row['Costo'], row['Piezas'], row['Total']))
+        else:
+            cur.execute("""
+                INSERT INTO inventario (Tipo, Gama, Articulo_Descripcion, Foto, Costo, Tarimas, Total)
+                VALUES (%s, %s, %s, %s, %s, %s, %s);
+            """, ("", row['Gama'], row['Articulo_Descripcion'], row['Foto'], row['Costo'], row['Tarimas'], row['Total']))
 
-# # Antes de crear la base de datos, borramos la base de datos en caso de que exista
-# cur.execute("DROP TABLE IF EXISTS inventario;")
+    conn.commit()
+    cur.close()
 
-# # Creamos el esquema
-# cur.execute('''
-#     CREATE TABLE inventario (
-#         id SERIAL PRIMARY KEY,
-#         Tipo VARCHAR(20), 
-#         Gama VARCHAR(255),
-#         Articulo_Descripcion VARCHAR(255),
-#         Foto VARCHAR(255),
-#         Costo DECIMAL(10,2),
-#         Piezas INTEGER,
-#         Total DECIMAL(10,2)
-#     );
-# ''')
-# # AÑADIR ESTO A TIPO YA QUE SE PUEDA LEER EL NOMBRE DE LA HOJA NOT NULL CHECK (Tipo IN ('MATERIA PRIMA', 'PROCESO', 'SABORES Y COLORES', 'PRODUCTO TERMINADO'))
-# # Rellenamos la base de datos 
+def read_database_table(conn):
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM inventario")
+    df = pd.read_sql('SELECT * FROM Inventario', conn)
+    cur.close()
 
-# for i, row in df.iterrows():
-#     cur.execute("""
-#         INSERT INTO inventario (Gama, Articulo_Descripcion, Foto, Costo, Piezas, Total)
-#         VALUES (%s, %s, %s, %s, %s, %s);
-#     """, (row['Gama'], row['Articulo_Descripcion'], row['Foto'], row['Costo'], row['Piezas'], row['Total']))
+    return df
 
-# # Subir los cambios a la base de datos
-# conn.commit()
+def main():
+    credentials_filename = 'Credentials.json'
+    url = 'https://docs.google.com/spreadsheets/d/1m0qPA23PW2tQL1rrStTBrN9H1SpMvPg-Y-HiWd0FLcE/edit?usp=sharing'
+    
+    #Obtenemos informacion del Google Sheet
+    dfs = get_spreadsheet_data(credentials_filename, url)
+    #Limpiamos los datos
+    for df in dfs:
+        clean_dataframe(dfs[df])
+    
+    #print(dfs['Materia Prima'])
+    #Conectamos a la base de datos
+    conn = psycopg2.connect(
+        host="pg.pg4e.com",
+        port=5432,
+        database="pg4e_d5cf41ba12", 
+        user="pg4e_d5cf41ba12",
+        password="pg4e_p_34432ea43bce4f4"
+    )
 
-# # Extraer los datos de la base de datos
-# cur.execute("SELECT * FROM inventario")
+    #Creamos la tabla en la base de datos
+    create_database_table(conn)
 
-# # extraemos los datos del objeto cur
-# dfB = pd.read_sql('SELECT * FROM Inventario', conn)
+    #Llenamos la tabla con los datos del Google Sheet
+    for df in dfs:
+        populate_database_table(conn, dfs[df])
+    
+    #Leemos la tabla de la base de datos
+    df = read_database_table(conn)
+    print(df)
 
-# conn.close()
-
-
+if __name__ == '__main__':
+    main()
+    
+    
